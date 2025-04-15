7a03b10f70206be96244d3f3cadc31fc509738ae
Reshma John <reshma.chackola@gmail.com>
2025-04-15 17:37:09 +0200

    Get Form values

---
+++ recipeKeeper.js
@@ -0,0 +1,27 @@
+let recipeForm = document.getElementById("recipe-form");
+let recipeName = document.getElementById("recipe-name");
+let ingredients = document.getElementById("ingredients");
+let steps = document.getElementById("steps");
+let displayArea = document.getElementById("display-area");
+
+let recipes = [];
+recipeForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let enteredRecipeName = recipeName.value;
+  let enteredIngredients = ingredients.value;
+  let enteredSteps = steps.value;
+
+  // create a new recipe
+  let newRecipe = {
+    name: enteredRecipeName,
+    ingredients: enteredIngredients,
+    steps: enteredSteps,
+  };
+  recipeName.value = "";
+  ingredients.value = "";
+  steps.value = "";
+
+  // add the new recipe to the recipes array
+
+  // We'll be adding more functionality here in the next steps.
+});
