3e2b29bde1b7f02b189c50a8c606f75a86ed513c
Reshma John <reshma.chackola@gmail.com>
2025-04-16 15:11:25 +0200

    added the delete button

--- recipeKeeper.js
+++ recipeKeeper.js
@@ -2,6 +2,7 @@ let recipeForm = document.getElementById("recipe-form");
 let recipeName = document.getElementById("recipe-name");
 let ingredients = document.getElementById("ingredients");
 let steps = document.getElementById("steps");
+let recipe_url = document.getElementById("recipe-image-url");
 let displayArea = document.getElementById("display-area");
 
 let recipes = [];
@@ -10,16 +11,19 @@ recipeForm.addEventListener("submit", function (event) {
   let enteredRecipeName = recipeName.value;
   let enteredIngredients = ingredients.value;
   let enteredSteps = steps.value;
+  let image_url = recipe_url.value;
 
   // create a new recipe
   let newRecipe = {
     name: enteredRecipeName,
     ingredients: enteredIngredients,
     steps: enteredSteps,
+    url: image_url,
   };
   recipeName.value = "";
   ingredients.value = "";
   steps.value = "";
+  recipe_url.value = "";
 
   displayRecipe(newRecipe);
 });
@@ -27,8 +31,20 @@ recipeForm.addEventListener("submit", function (event) {
 function displayRecipe(recipe) {
   // create a div for the new recipe
   let recipeDiv = document.createElement("div");
-  console.log(recipe);
+  // create a delete button
+  let deleteButton = document.createElement("button");
+  if (recipe.url) {
+    let imgElement = document.createElement("img");
+    imgElement.src = recipe.url;
+    imgElement.style.maxWidth = "200px";
+    recipeDiv.appendChild(imgElement);
+  }
   recipeDiv.innerHTML += `<h1>${recipe.name}</h1> Ingredients:  ${recipe.ingredients}<p></p> Steps<p>${recipe.steps}</p>`;
+  recipeDiv.appendChild(deleteButton);
   displayArea.appendChild(recipeDiv);
   displayArea.style.display = "block";
+  deleteButton.onclick = function () {
+    recipeDiv.remove();
+    displayArea.style.display = "none";
+  };
 }
